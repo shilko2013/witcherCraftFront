43acb50254304bacf03d5b141f7f25f497f5215b
shilko2013 <shilko2013@mail.ru>
2019-01-05 20:27:46 +0300

    logout ready.

---
+++ src/app/components/AsideMenu/LogoutNode/LogoutNode.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import './LogoutNode.css';
+import defaultImg from "../../../resources/images/defaultImg.bmp";
+import connect from "react-redux/es/connect/connect";
+import {logout} from "../../../actions/AuthActions";
+
+class LogoutNode extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      src: props.src,
+      text: props.text
+    }
+  }
+  render(){
+    return (
+        <div className="MenuNode">
+          <img
+              src={this.state.src || defaultImg}
+              alt=""/>
+          <div className="nodeText" onClick={this.props.logout}>
+            {this.state.text}
+          </div>
+        </div>
+    )
+  }
+}
+
+export default connect(
+    state => ({}),
+    dispatch => ({
+      logout: () => {
+        dispatch(logout());
+      }
+    })
+)(LogoutNode);
