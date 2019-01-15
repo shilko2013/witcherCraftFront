b9a2ac275f4197e8cba7da8d9656c3e8c776df98
shilko2013 <shilko2013@mail.ru>
2019-01-15 09:03:47 +0300

    Ingredient add page added.

---
+++ src/app/pages/RecipesPage/RecipesPage.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import './RecipesPage.css';
+
+class RecipesPage extends Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      <div className="RecipesPage">
+    
+      </div>
+    )
+  }
+}
+RecipesPage.propTypes = {
+}
+
+export default RecipesPage;
